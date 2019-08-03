fad1a597a6d41a0743978681b35b8224e40edb5f
anat <gilenson.anat@gmail.com>
2019-08-04 02:16:55 +0300

    Imrove HTML with HTML inheritance

--- megilot.py
+++ megilot.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def mainPage():
     return render_template('index.html')
+
+@app.route('/results')
+def searchResult():
+    return render_template('results.html', title='Search Results', header ='תוצאות חיפוש')
 
 if __name__=='__main__':
     app.run(debug=True)
