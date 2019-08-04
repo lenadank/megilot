4845e8ccb37535fa10c269410f19cb6df3aaf14f
anat <gilenson.anat@gmail.com>
2019-08-04 04:13:58 +0300

    Change some vairable names

--- megilot.py
+++ megilot.py
@@ -5,13 +5,14 @@ app = Flask(__name__)
 def mainPage():
     return render_template('index.html')
 
-@app.route('/results',methods=['POST'])
+@app.route('/results')
 def searchResult():
     first_let = request.form['first-letters']
     last_let = request.form['last-letters']
-    text_len = request.form['text-length']
-    url = request.form['text-url']
-    return render_template('results.html', title='Search Results', header ='תוצאות חיפוש',first_letters=first_let, last_letters=last_let, text_length=text_len, text_url = url)
+    txt_len = request.form['text-length']
+    txt_url = request.form['text-url']
+
+    return render_template('results.html', title='Search Results', header ='תוצאות חיפוש',first_letters=first_let, last_letters=last_let, text_length=txt_len, text_url = txt_url)
 
 if __name__=='__main__':
     app.run(debug=True)
