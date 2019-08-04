fb6bffd155eca2b081369b675781d5ad6bb9b74c
anat <gilenson.anat@gmail.com>
2019-08-04 03:47:44 +0300

    Add redirect to /results when "search" butten is clicked and echo the user's input in the new page.

--- megilot.py
+++ megilot.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
 def mainPage():
     return render_template('index.html')
 
-@app.route('/results')
+@app.route('/results',methods=['POST'])
 def searchResult():
-    return render_template('results.html', title='Search Results', header ='תוצאות חיפוש')
+    first_let = request.form['first-letters']
+    last_let = request.form['last-letters']
+    text_len = request.form['text-length']
+    url = request.form['text-url']
+    return render_template('results.html', title='Search Results', header ='תוצאות חיפוש',first_letters=first_let, last_letters=last_let, text_length=text_len, text_url = url)
 
 if __name__=='__main__':
     app.run(debug=True)
