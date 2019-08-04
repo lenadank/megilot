dd77016f2c7fa5d422a964a6a7954cc2eb988ae9
anat <gilenson.anat@gmail.com>
2019-08-05 00:10:49 +0300

    Move all stryle code from HTML files to style.css. Add cards with text in results page.

--- megilot.py
+++ megilot.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 from urllib.parse import urlencode, parse_qs
 app = Flask(__name__)
 
+lines = [{'title': "Hey", 'text': "The well-documented monihvkhvkhdvhckvsdkcvdskjcvsdjlvcdljhcvdlcvhdcjlvdsljcvdsjcvsdhcvsdjlvcdschvsljcvlhsdvcjlsdvcdsvcjlvdscjvdscljvdschjsvdcljsvhsvlchvsdljvdscljvcjsdvclsdvcljsvdcjhvscljshtor backs into the incomplete bed. What if the flashy quarter ate the dress?"}, {'title': "How", 'text': "The well-documented monitor backs into the incomplete bed. What if the flashy quarter ate the dress?"}, {'title': "Hi", 'text': "The well-documented monitor backs into the incomplete bed. What if the flashy quarter ate the dress?"},
+         {'title': "Bye", 'text': "The well-documented monitor backs into the incomplete bed. What if the flashy quarter ate the dress?"}, {'title': "Bay", 'text': "The well-documented monitor backs into the incomplete bed. What if the flashy quarter ate the dress?"}, {'title': "Bow", 'text': "The well-documented monitor backs into the incomplete bed. What if the flashy quarter ate the dress?"}]
+
+
 @app.route('/searching', methods=['POST'])
 def search():
     if request.method == 'POST':
@@ -9,6 +13,7 @@ def search():
                          'txt_length': request.form['text-length'], 'txt_url': request.form['text-url']}
         search_path = urlencode(search_params)
         return redirect(url_for('searchResult', search_path=search_path))
+
 
 @app.route('/', methods=['GET'])
 def mainPage():
@@ -23,7 +28,7 @@ def searchResult(search_path):
     last_letters = search_params['last_letters'][0]
     txt_length = search_params['txt_length'][0]
     txt_url = search_params['txt_url'][0]
-    return render_template('results.html', first_letters=first_letters, last_letters=last_letters, txt_length=txt_length, txt_url=txt_url, title='Search Results', header="תוצאות חיפוש")
+    return render_template('results.html', first_letters=first_letters, last_letters=last_letters, txt_length=txt_length, txt_url=txt_url, title='Search Results', header="תוצאות חיפוש", lines=lines)
 
 
 if __name__ == '__main__':
